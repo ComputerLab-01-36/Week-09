e34d45c692404a6f6d962eed5069b1072ae4b2af
Toeyty <61030236@kmitl.ac.th>
2019-11-10 14:53:05 +0700

    vb

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,29 @@ namespace GDIPlus_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Dispose();
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            LinearGradientBrush pnlGdt = new LinearGradientBrush(panel1.ClientRectangle,
+                Color.Yellow, Color.Navy, 0f, true);
+            e.Graphics.FillRectangle(pnlGdt, panel1.ClientRectangle);
+            pnlGdt.Dispose();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            LinearGradientBrush pnlGdt = new LinearGradientBrush(panel2.ClientRectangle,
+                Color.Yellow, Color.Navy, 90f, true);
+            e.Graphics.FillRectangle(pnlGdt, panel2.ClientRectangle);
+            pnlGdt.Dispose();
         }
     }
 }
