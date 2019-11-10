c53d6b9c2b00eb6a3a954d8929304a1a6e1978c6
Toeyty <61030236@kmitl.ac.th>
2019-11-10 16:02:15 +0700

    fytut

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace GDIPlus_1
 {
@@ -16,5 +17,31 @@ namespace GDIPlus_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Bitmap bmp = new Bitmap("D:\\1.JPG");
+            this.SetClientSizeCore(bmp.Width ,bmp.Height);
+            Rectangle topLeft = new Rectangle(0, 0, bmp.Width / 2, bmp.Height / 2);
+            Rectangle topRight = new Rectangle(bmp.Width / 2, 0, bmp.Width / 2, bmp.Height / 2);
+            Rectangle bottomLeft = new Rectangle(0, bmp.Height / 2, bmp.Width / 2, bmp.Height / 2);
+            Rectangle bottonRight = new Rectangle(bmp.Width / 2, bmp.Height / 2, bmp.Width / 2, bmp.Height / 2);
+
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipNone);
+            e.Graphics.DrawImage(bmp, topLeft);
+
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            e.Graphics.DrawImage(bmp, topRight);
+
+            bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            e.Graphics.DrawImage(bmp, bottomLeft);
+
+            bmp.RotateFlip(RotateFlipType.Rotate180FlipY);
+            e.Graphics.DrawImage(bmp, bottonRight);
+            g.Dispose();
+
+        }
+
     }
 }
