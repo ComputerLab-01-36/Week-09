5e007131f3d575f4dfefc6b698caff88c67df359
Toeyty <61030236@kmitl.ac.th>
2019-11-10 12:55:54 +0700

    ad

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,16 @@ namespace GDIPlus_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            HatchBrush mybrush = new HatchBrush(HatchStyle.DarkVertical, Color.White, Color.Violet);
+            Pen mypen = new Pen(mybrush, 5);
+            g.DrawEllipse(mypen, 10, 10, 200, 200);
+            mypen.Dispose();
+            mybrush.Dispose();
         }
     }
 }
