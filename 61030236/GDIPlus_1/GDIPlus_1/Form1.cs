8e869471bb9fad5bd44d81c6d2b18695e2a3c1cd
Toeyty <61030236@kmitl.ac.th>
2019-11-10 14:38:16 +0700

    fh

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -12,9 +12,23 @@ namespace GDIPlus_1
 {
     public partial class Form1 : Form
     {
+        private Brush brush;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Image image = Image.FromFile("d:Capture.PNG");
+            // Change "\\d:Capture.PNG" to match your image path and name.
+            TextureBrush bruch = new TextureBrush(image);
+            Rectangle rect = new Rectangle(10, 10, 180, 150);
+            g.FillEllipse(brush, rect);
+            g.Dispose();
+
         }
     }
 }
