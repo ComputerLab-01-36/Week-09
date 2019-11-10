d0ddd5f35db94c22b172e88d904e369e593b9df4
Toeyty <61030236@kmitl.ac.th>
2019-11-10 14:09:28 +0700

    bn

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,24 @@ namespace GDIPlus_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            //Draw reference ellipse with dash line.
+            Pen penEllipse = new Pen(Color.Brown);
+            penEllipse.DashStyle = DashStyle.Dash;
+            g.DrawEllipse(penEllipse, 20, 20, 200, 150);
+
+            //Draw pie
+            Pen penPie = new Pen(Color.Magenta, 2);
+            g.DrawPie(penPie, 20, 20, 200,150, 45, 90);
+            Pen penPie1 = new Pen(Color.BlueViolet, 2);
+            g.DrawPie(penPie1, 20, 20, 200, 150, 150, 45);
+
+            g.Dispose();
+
         }
     }
 }
