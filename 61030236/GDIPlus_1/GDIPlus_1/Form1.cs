325a9e1f43abe554f17448a80919a4762dc2efce
Toeyty <61030236@kmitl.ac.th>
2019-11-10 12:05:47 +0700

    commit

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
 
 namespace GDIPlus_1
 {
@@ -15,6 +17,37 @@ namespace GDIPlus_1
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen bluepen = new Pen(Color.Blue, 2);
+            g.DrawRectangle(bluepen, 10, 10, 100, 100);
+            bluepen.Dispose();
+
+            Pen SomePen = new Pen(Color.FromArgb(255, 120, 200));
+            g.DrawEllipse(SomePen, 20, 20, 200, 200);
+            SomePen.Dispose();
+
+            Color col = Color.FromName("LightGreen");
+            this.BackColor = col;
+
+
+        }
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen bluepen = new Pen(Color.Blue, 2);
+            g.DrawRectangle(bluepen, 10, 10, 100, 100);
+            bluepen.Dispose();
+
+            Pen SomePen = new Pen(Color.FromArgb(255, 120, 200));
+            g.DrawEllipse(SomePen, 20, 20, 200, 200);
+            SomePen.Dispose();
+
+            Color col = Color.FromName("LightGreen");
+            this.BackColor = col;
         }
     }
 }
