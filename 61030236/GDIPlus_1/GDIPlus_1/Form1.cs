3810fb8f5d21de2276a42230f0211b5a17804ba5
Toeyty <61030236@kmitl.ac.th>
2019-11-10 14:22:48 +0700

    com

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
 
 namespace GDIPlus_1
 {
@@ -15,6 +17,23 @@ namespace GDIPlus_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Point[] pt =
+            {
+                new Point(10,20),
+                new Point(188,246),
+                new Point(250,192),
+                new Point(220,48),
+
+            };
+            g.FillClosedCurve(Brushes.Blue, pt);
+            g.DrawClosedCurve(Pens.Red, pt);
+            g.Dispose();
+
         }
     }
 }
