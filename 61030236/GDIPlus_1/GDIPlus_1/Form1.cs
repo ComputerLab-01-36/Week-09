26ba53f338c34b219c44eaa4424d7e0ce68f4df6
Toeyty <61030236@kmitl.ac.th>
2019-11-10 13:36:33 +0700

    fy

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,31 @@ namespace GDIPlus_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            GraphicsPath gp = new GraphicsPath();
+
+            gp.AddCurve(new Point[]{
+                new Point(100,50),
+                new Point(105,40),
+                new Point(120,40),
+                new Point (130,65),
+                new Point (100,100) }, 0.5f);
+
+
+            gp.AddCurve(new Point[] { new Point(100,100),
+                new Point(70,65),
+            new Point (80,40),
+            new Point(95,40),
+            new Point(100,50)}, 0.5f);
+
+            g.DrawPath(new Pen(Color.Red, 3), gp);
+            g.Dispose();
+        }
+
+
     }
 }
+
