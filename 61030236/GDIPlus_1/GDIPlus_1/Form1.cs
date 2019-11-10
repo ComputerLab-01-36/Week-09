dc4882a889850b60d0a96934d650f029d1e26133
Toeyty <61030236@kmitl.ac.th>
2019-11-10 13:06:01 +0700

    hhhj

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,20 @@ namespace GDIPlus_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Green, 2);
+            pen.DashStyle = DashStyle.DashDot;
+            e.Graphics.DrawLine(pen, 10, 80, 220, 80);
+            pen.Dispose();
+            pen = new Pen(Brushes.DeepSkyBlue, 4);
+            e.Graphics.DrawLine(pen, 10, 120, 220, 120);
+            pen.Dispose();
+
+
         }
     }
 }
