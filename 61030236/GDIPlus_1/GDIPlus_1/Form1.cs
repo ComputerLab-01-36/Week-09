088fefa5d0b900315a4122f43839eaf01e608897
Toeyty <61030236@kmitl.ac.th>
2019-11-10 14:31:21 +0700

    com

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,27 @@ namespace GDIPlus_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            this.SetClientSizeCore(500, 600);
+            Graphics g = e.Graphics;
+            HatchBrush brush;
+            int x = 20;
+            int y = 20;
+            foreach (HatchStyle brushStyle in Enum.GetValues(typeof(HatchStyle)))
+            {
+                brush = new HatchBrush(brushStyle, Color.Navy, Color.Yellow);
+                g.FillRectangle(brush, x, y, 40, 20);
+                y += 30;
+                if ((y+30) > this.ClientSize.Height)
+                {
+                    y = 20;
+                    x += 150;
+                }
+            }
+            g.Dispose();
         }
     }
 }
