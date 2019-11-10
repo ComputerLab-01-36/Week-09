01cb64ca9df56adaf04f5245ebd1e71664726922
Toeyty <61030236@kmitl.ac.th>
2019-11-10 13:47:54 +0700

    commit

--- 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
+++ 61030236/GDIPlus_1/GDIPlus_1/Form1.cs
@@ -16,5 +16,21 @@ namespace GDIPlus_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Color.Blue, 3);
+            Rectangle[] rects =
+            {
+                new Rectangle(20,20,120,120),
+            new Rectangle(20,50,120,30),
+            new Rectangle(20,90,120,40),
+            new Rectangle(20, 140, 120, 60)};
+
+            g.DrawRectangles(myPen, rects);
+
+            g.Dispose();
+        }
     }
 }
